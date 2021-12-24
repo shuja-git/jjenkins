2b381bd0ab0c66a721e4ff3eb410a5d362b3966e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-24 06:29:47 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -1,16 +1,41 @@
-job('example') {
-  logRotator(-1, 10)
-  jdk('Java 8')
-  scm {
-    github('jenkinsci/job-dsl-plugin', 'master')
-  }
-  triggers {
-    githubPush()
-  }
-  steps {
-    gradle('clean build')
-  }
-  publishers {
-    archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
+//pipelineJob('frontend') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/frontend')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
+
+pipelineJob('roboshop-ansible') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/raghudevopsb61/ansible.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
   }
 }
