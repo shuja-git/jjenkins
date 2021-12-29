d4a0fb84d95000c91b239237375f075390b3a292
shuja <shuja.devops@gmail.com>
2021-12-29 23:11:17 +0530

    call method

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,43 @@
-def info(message) {
-  echo "INFO: ${message}"
+def call(){
+  pipeline {
+    agent { label 'WORKSTATION' }
+
+    triggers { pollSCM('*/2 * * * *') }
+
+    stages {
+
+      stage('compile the code') {
+        steps {
+          sh 'echo compile the code'
+
+        }
+      }
+      stage('check the code quality') {
+        steps {
+          sh 'echo check the code quality'
+        }
+      }
+      stage('Test cases') {
+        steps {
+          sh 'echo Test cases'
+        }
+      }
+    }
+  }
+
+
 }
 
-def warning(message) {
-  echo "WARNING: ${message}"
-}
+
+
+//----------------------
+//def info(message) {
+//  echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//  echo "WARNING: ${message}"
+//}
 
 
 //--------------------------------------
