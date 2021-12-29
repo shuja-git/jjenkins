b05bc0d42e1450e0cf315a2f5afd63e605caeddd
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-29 06:42:05 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -45,23 +45,30 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-pipelineJob('CI-Pipelines/cart') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/raghudevopsb61/cart.git')
+def COMPONENTS = ["cart", "catalogue"]
+
+for(COMPONENT in COMPONENTS) {
+  pipelineJob("CI-Pipelines/${COMPONENT}") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'("https://github.com/raghudevopsb61/${COMPONENT}.git")
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
           }
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
       }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
     }
   }
 }
+
+
+
