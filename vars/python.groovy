f9a78080512f19d84d0f995e1c23ba283dae5b89
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-29 06:38:49 +0530

    Update jenkins/

---
+++ vars/python.groovy
@@ -0,0 +1,34 @@
+def call() {
+  pipeline {
+    agent {
+      label "${BUILD_LABEL}"
+    }
+
+    triggers {
+      pollSCM('*/2 * * * *')
+    }
+
+    stages {
+
+      stage('Compile the Code') {
+        steps {
+          sh 'echo Compile the ${COMPONENT} Code'
+        }
+      }
+
+      stage('Check the Code Quality') {
+        steps {
+          sh 'echo Check the code Quality'
+        }
+      }
+
+      stage('Test Cases') {
+        steps {
+          sh 'echo Test Cases'
+        }
+      }
+
+    }
+
+  }
+}
