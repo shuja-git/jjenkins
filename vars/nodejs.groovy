ab30b7bbea1bd50b1da05da56ba6e4689a86aaa4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-29 06:25:05 +0530

    Update jenkins/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,32 +1,34 @@
-pipeline {
-  agent {
-    label 'WORKSTATION'
-  }
-
-  triggers {
-    pollSCM('*/2 * * * *')
-  }
-
-  stages {
-
-    stage('Compile the Code') {
-      steps {
-        sh 'Compile the Code'
-      }
+def call() {
+  pipeline {
+    agent {
+      label 'WORKSTATION'
     }
 
-    stage('Check the Code Quality') {
-      steps {
-        sh 'echo Check the code Quality'
-      }
+    triggers {
+      pollSCM('*/2 * * * *')
     }
 
-    stage('Test Cases') {
-      steps {
-        sh 'echo Test Cases'
+    stages {
+
+      stage('Compile the Code') {
+        steps {
+          sh 'Compile the Code'
+        }
       }
+
+      stage('Check the Code Quality') {
+        steps {
+          sh 'echo Check the code Quality'
+        }
+      }
+
+      stage('Test Cases') {
+        steps {
+          sh 'echo Test Cases'
+        }
+      }
+
     }
 
   }
-
 }
